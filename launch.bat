ce179471800dfea5ae422cc206101250c478867a
Rui Teixeira <ruiteixeira737@gmail.com>
2023-05-24 21:22:12 +0100

    Saved world

--- launch.bat
+++ launch.bat
@@ -1,1 +1,12 @@
-java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
+@echo off
+
+java -Xmx3G -Xms1G -jar minecraft_server.jar nogui
+
+echo Add
+powershell -Command "git add ."
+echo Commit
+powershell -Command "git commit -m 'Saved world'"
+echo Push
+powershell -Command "git push origin Vanilla"
+PAUSE
+
